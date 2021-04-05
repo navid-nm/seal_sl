b94a090a8488e55527c7643a5b93c34e61f0a6f9
Navid <navidmomtahen@gmail.com>
2021-04-05 21:09:27 +0100

    Added event loop

--- seal.cpp
+++ seal.cpp
@@ -3,15 +3,32 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <list>
 #include "lib/StatusManager.hpp"
 
 using namespace std;
 StatusManager sm;
 
+void processcommand(string command) {
+  const regex say("^(?:say\())");
+  auto wrdbegin = sregex_iterator(command.begin(), command.end(), say);
+  auto wrdend = sregex_iterator();
+  int pos = command.find('(') + 1;
+  if (distance(wrdbegin, wrdend) == 1) {
+    string mouth = command.substr(pos, command.find(')', pos) - pos);
+    printf("%s\n", mouth.c_str());
+  }
+}
+
+void execflow(list<string> list) {
+  for (auto i : list) {
+    processcommand(i);
+  }
+}
+
 int main(int argc, char* argv[]) {
   system("clear");
-  const regex say("^(?:say\())");
-
+  list<string> instructions;
   fstream fstr;
   fstr.open(argv[1]);
   if (fstr.fail()) {
@@ -19,15 +36,9 @@ int main(int argc, char* argv[]) {
   } else {
     string line;
     while (getline(fstr, line)) {
-      auto wrdbegin = sregex_iterator(line.begin(), line.end(), say);
-      auto wrdend = sregex_iterator();
-      auto pos = line.find('(') + 1;
-      if (distance(wrdbegin, wrdend) == 1) {
-        string mouth = line.substr(pos, line.find(')', pos) - pos);
-        printf("%s\n", mouth.c_str());
-      }
+      instructions.push_back(line);
     }
+    execflow(instructions);
   }
-    
   return 0;
 }
