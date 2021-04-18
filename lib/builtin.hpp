9afd23071dccd136e6213eb7c4b063250b2105cc
Navid <navidmomtahen@gmail.com>
2021-04-18 11:15:37 +0100

    Added groundwork for implementation of variables

--- lib/builtin.hpp
+++ lib/builtin.hpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <string>
 
+enum type {INT, STR, SWITCH};
+
 class builtin {
   public:
     void say(std::string text);
     void sleep(int time);
+};
+
+class var {
+  public:
+    void gettype();
+    void set(std::string val);
 };
